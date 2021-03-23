f1bdf5d3179b30cbd54e97892416065b12a22a9c
Ryan Lim <ryanlim7392@gmail.com>
2021-03-23 20:57:44 +0800

    customer entity update

--- EasyAppointmentLibrary/src/entity/CustomerEntity.java
+++ EasyAppointmentLibrary/src/entity/CustomerEntity.java
@@ -8,6 +8,7 @@ package entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -15,7 +16,151 @@ import javax.persistence.Id;
  */
 public class CustomerEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String indentityNo;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private Integer age;
+    private String phone;
+    private String address;
+    private String city;
+    private String emailAddress;
     
+    @OneToMany
+    private AppointmentEntity appointmentEntity;
+
+    public CustomerEntity() {
+    }
+
+    public CustomerEntity(Long id, String indentityNo, String password, String firstName, String lastName, String gender, Integer age, String phone, String address, String city, String emailAddress) {
+        this();
+        
+        this.id = id;
+        this.indentityNo = indentityNo;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.emailAddress = emailAddress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIndentityNo() {
+        return indentityNo;
+    }
+
+    public void setIndentityNo(String indentityNo) {
+        this.indentityNo = indentityNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CustomerEntity)) {
+            return false;
+        }
+        CustomerEntity other = (CustomerEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.CustomerEntity[ id=" + id + " ]";
+    }
 }
