e6ea03a529e26fc15506d841abfef79a8728647b
bulldoggy <ivanlim97@hotmail.com>
2021-03-26 00:26:42 +0800

    Added ServiceProviderEntitySessionBeanLocal and their relevant methods.

---
+++ EasyAppointment-ejb/src/java/ejb/session/stateless/ServiceProviderEntitySessionBeanLocal.java
@@ -0,0 +1,24 @@
+package ejb.session.stateless;
+
+import entity.ServiceProviderEntity;
+import util.exception.InvalidLoginCredentialException;
+import util.exception.ServiceProviderNotFoundException;
+
+/**
+ *
+ * @author user
+ */
+public interface ServiceProviderEntitySessionBeanLocal{
+    
+    public Long createServiceProviderEntity(ServiceProviderEntity newServiceProviderEntity);
+    
+    public ServiceProviderEntity retrieveServiceProviderByUniqueIdNumber(Long uniqueIdNumber) throws ServiceProviderNotFoundException ;
+    
+    public ServiceProviderEntity retrieveServiceProviderByEmail(String emailAddress) throws ServiceProviderNotFoundException;
+    
+    public ServiceProviderEntity serviceProviderLogin(String emailAddress, String password) throws InvalidLoginCredentialException;
+
+    public void updateServiceProviderEntity(ServiceProviderEntity serviceProviderEntity);
+    
+    public void deleteServiceProviderEntity(Long uniqueIdNumber) throws ServiceProviderNotFoundException;
+}
