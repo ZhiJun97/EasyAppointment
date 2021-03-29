990d0cf49265f2a3d5639f5ac10339bd785ba9f3
bulldoggy <ivanlim97@hotmail.com>
2021-03-30 00:19:12 +0800

    Added DataInitSessionBean for data creation upon initialization.

---
+++ EasyAppointment-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -0,0 +1,55 @@
+package ejb.session.singleton;
+
+import ejb.session.stateless.AdminEntitySessionBeanLocal;
+import ejb.session.stateless.AppointmentEntitySessionBeanLocal;
+import ejb.session.stateless.ServiceProviderEntitySessionBeanLocal;
+import entity.AdminEntity;
+import entity.AppointmentEntity;
+import entity.ServiceProviderEntity;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
+import util.exception.AdminNotFoundException;
+
+@Singleton
+@LocalBean
+@Startup
+public class DataInitSessionBean {
+
+    @EJB(name = "AdminEntitySessionBeanLocal")
+    private AdminEntitySessionBeanLocal adminEntitySessionBeanLocal;
+    @EJB(name = "AppointmentEntitySessionBeanLocal")
+    private AppointmentEntitySessionBeanLocal appointmentEntitySessionBeanLocal;
+    @EJB(name = "ServiceProviderEntitySessionBeanLocal")
+    private ServiceProviderEntitySessionBeanLocal serviceProviderEntitySessionBeanLocal;
+    
+    public DataInitSessionBean()
+    {
+    }
+    
+    @PostConstruct
+    public void postConstruct()
+    {
+        try
+        {
+            adminEntitySessionBeanLocal.retrieveAdminByEmail("admin@easyappt.com");
+        }
+        catch(AdminNotFoundException ex)
+        {
+            initializeData();
+        }
+    }
+    
+    
+    
+    private void initializeData()
+    {  
+        adminEntitySessionBeanLocal.createAdminEntity(new AdminEntity("admin@easyadm.com", "password"));
+        
+        serviceProviderEntitySessionBeanLocal.createServiceProviderEntity(new ServiceProviderEntity("A1101", "Restaurant", "John's Special", "Airport Road, Avenue 6", "Singapore", "john@easysp.com", "password"));
+        
+        //require customer entity session bean to initialize customer data
+    }
+}
