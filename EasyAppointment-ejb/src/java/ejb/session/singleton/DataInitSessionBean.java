815f299efbc488b0feba457f4707b3daadfed8dc
bulldoggy <ivanlim97@hotmail.com>
2021-03-31 19:51:58 +0800

    Updated customer data to be initialized and fixed error with service provider creation.

--- EasyAppointment-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
+++ EasyAppointment-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -2,9 +2,11 @@ package ejb.session.singleton;
 
 import ejb.session.stateless.AdminEntitySessionBeanLocal;
 import ejb.session.stateless.AppointmentEntitySessionBeanLocal;
+import ejb.session.stateless.CustomerEntitySessionBeanLocal;
 import ejb.session.stateless.ServiceProviderEntitySessionBeanLocal;
 import entity.AdminEntity;
 import entity.AppointmentEntity;
+import entity.CustomerEntity;
 import entity.ServiceProviderEntity;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -50,8 +52,8 @@ public class DataInitSessionBean {
     {  
         adminEntitySessionBeanLocal.createAdminEntity(new AdminEntity("admin@easyadm.com", "password"));
         
-        serviceProviderEntitySessionBeanLocal.createServiceProviderEntity(new ServiceProviderEntity("A1101", "Restaurant", "John's Special", "Airport Road, Avenue 6", "Singapore", "john@easysp.com", "password"));
+        serviceProviderEntitySessionBeanLocal.createServiceProviderEntity(new ServiceProviderEntity("A1101", "Restaurant", "John's Special", "Airport Road, Avenue 6", "Singapore", "john@easysp.com", "91234567", "password"));
         
-        //require customer entity session bean to initialize customer data
+        customerEntitySessionBeanLocal.createCustomerEntity(new CustomerEntity("S1234567F", "password", "Adam", "Tan", "Male", 23, "82345678", "Yishun Ring Road", "Singapore", "adam@easycust.com"));
     }
 }
